292066efae157a63e7717681a2f3f164d7ae5065
Matt Ellis <m.t.ellis@gmail.com>
2014-05-12 20:30:41 +0100

    Updates

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+var path = require("path");
+
+module.exports = function (grunt) {
+    grunt.loadNpmTasks('grunt-gitbook');
+    grunt.loadNpmTasks('grunt-gh-pages');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+
+    grunt.initConfig({
+        'gitbook': {
+            development: {
+                output: path.join(__dirname, ".grunt/gitbook"),
+                input: "./",
+                title: "resharper-devguide",
+                description: "development-guide-for-resharper",
+                github: "citizenmatt/resharper-devguide"
+            }
+        },
+        'gh-pages': {
+            options: {
+                base: '.grunt/gitbook'
+            },
+            src: ['**']
+        },
+        'clean': {
+            files: '.grunt'
+        }
+    });
+
+    grunt.registerTask('publish', [
+        'gitbook',
+        'gh-pages',
+        'clean'
+    ]);
+    grunt.registerTask('default', 'gitbook');
+};
