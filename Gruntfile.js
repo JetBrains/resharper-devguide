aca76145052ea6505e99b9adff2890575d24fc9a
Matt Ellis <m.t.ellis@gmail.com>
2014-08-04 21:05:14 +0100

    Updates

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+var path = require("path");
+
+module.exports = function (grunt) {
+    grunt.loadNpmTasks('grunt-gitbook');
+    grunt.loadNpmTasks('grunt-gh-pages');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+
+    grunt.initConfig({
+        'gitbook': {
+            development: {
+                output: path.join(__dirname, ".grunt/gitbook"),
+                input: "./"
+            }
+        },
+        'gh-pages': {
+            options: {
+                base: '.grunt/gitbook'
+            },
+            src: ['**']
+        },
+        'clean': {
+            files: '.grunt'
+        }
+    });
+
+    grunt.registerTask('publish', [
+        'gitbook',
+        'gh-pages',
+        'clean'
+    ]);
+    grunt.registerTask('default', 'gitbook');
+};
